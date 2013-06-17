ab06cc85a99558f7016c463a517ef8576cd69910
Martin Popov <popov.marto@gmail.com>
2013-06-17 22:38:00 +0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to airplanes
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/airplanes/core_test.clj
@@ -0,0 +1,7 @@
+(ns airplanes.core-test
+  (:use clojure.test
+        airplanes.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
