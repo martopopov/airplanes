c2879fda433a6c29b5295c75e9d9f8658df0bca0
Martin Popov <popov.marto@gmail.com>
2013-07-11 20:36:57 +0300

    tests

--- test/airplanes/logic_test.clj
+++ test/airplanes/logic_test.clj
@@ -8,7 +8,7 @@
 
 (def airplanes-for-test (vector (agent (Airplane. (Coords. 2 2) (Coords. 1 0)))
                                 (agent (Airplane. :landed (Coords. 1 0)))
-                                (agent (Airplane. (Coords. 1 0) (Coords. 1 -1)))))
+                                (agent (Airplane. (Coords. 5 4) (Coords. 1 -1)))))
 
 (deftest test-logic
   (is (= (Airplane. (Coords. 6 4) (Coords. 1 -1))
@@ -26,5 +26,24 @@
       "remove landed airplanes")
   (is (= 4
          (count (add-airplane airplanes-for-test (* 100 new-plane-time))))
-      "there is a new plane, yey"))
+      "there is a new plane, yey")
+  (let [airplane @(peek (add-airplane airplanes-for-test (* 50 new-plane-time)))
+        direction (.direction airplane)
+        x-coords (.x (.coords airplane))
+        y-coords (.y (.coords airplane))]
+    (is (not= direction
+              (Coords. 0 0))
+      "they don't stay static")
+    (is (or (= x-coords (inc dim))
+            (= x-coords 0)
+            (= y-coords (inc dim))
+            (= y-coords 0))
+      "they are on the boundaries"))
+  (let [airplane (peek airplanes-for-test)]
+    (fly-update airplane)
+    (is (and (= (Airplane. (Coords. 6 3) (Coords. 1 -1))
+                @airplane)
+             (busy? (cell (Coords. 6 3)))
+             (free? (cell (Coords. 5 4))))
+      "they update")))
 
