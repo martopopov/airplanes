b026f70a3dc3e63e94152f30a2a78f53513412e7
Martin Popov <popov.marto@gmail.com>
2013-07-11 19:07:14 +0300

    Revert "deps"
    This reverts commit 1aa19301bade4e784888817e797cf6c5fa7f7ad1.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [seesaw "1.4.2"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [seesaw "1.4.2"]
+                 [clj-time "0.5.1"]]
   :main airplanes.core)
